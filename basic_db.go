877ce977c527cec9dd026bc10a4e4ffbd397f528
Jason Bornhoft <jmvbxx@gmail.com>
2018-08-31 18:28:26 -0500

    removed creds from file

--- basic_db.go
+++ basic_db.go
@@ -9,7 +9,7 @@ import (
 // based on http://mindbowser.com/golang-go-database-sql/
 
 func main() {
-	db, err := sql.Open("mysql", "root:password@tcp(172.17.0.2:3306)/UserDirectory")
+	db, err := sql.Open("mysql", "<user>:<password>@tcp(<ip>:3306)/UserDirectory")
 	if err != nil {
 		fmt.Println(err)
 	} else {
